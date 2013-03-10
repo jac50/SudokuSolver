f634e53405fd34c297ac8ac9d0d953375d5fbbb5
James Cox <james.a.cox91@googlemail.com>
2013-03-10 15:10:06 +0000

    Version 1.10 Changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,16 @@
 ChangeLog for Sudoku Solver
 ===========================
 
-Version 1.0.0 (Feburary 3rd, 2013)
------------
+Version 1.1.0 (March 10th, 2013)
+--------------------------------
+
++ Removed test comments and redundant code.
++ Removed testGrid to reduce the unneccessary copying of the grid.
++ Removed Check9
++ Tidied code
++ Removed GetRow as memcopy is used instead
+Version 1.0.0 (March 9th, 2013)
+-------------------------------
 
 + Initial Release
 
--- Sudoku.c
+++ Sudoku.c
@@ -3,43 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /*----- Global Declarations ----- */
 int tempGrid[9][9];
 int grid[9][9];
 char flag,flag2;
+
 /*----- Function Declarations -----*/
 void populateGrid(void);
 void output(void);
-int compare(const int * a,const int * b);
-char check9(int []);
 void inputSudoku(void);
-void getRow(int , int *);
-void getCol(int, int *);
-void getBox(int , int , int *);
 void solveSudoku(void);
 int getCandidates(int, int, int * );
-// char gridCheck(void);
-int bruteForce(int ,int );
+void getCol(int, int *);
+void getBox(int , int , int *);
+
+
 int getNextBlank(int row,int col, int *nextPos);
 void setTempGrid();
 void intro();
+
 /* ----- Start of Main ----- */
+
 int main(void){
-    int flag;
     intro();
     inputSudoku();
     setTempGrid();
 
     printf("\n\n\n\n");
     solveSudoku();
-    // if (gridCheck() == 'T'){printf("\n Puzzle has been solved.");}
     getchar();
     return 0;
 }
-void populateGrid(void){
 /* ----- Populate the Sudoku Grid with Zero's ----- */
-int i=0,j=0;
+void populateGrid(void){
+
+	int i=0,j=0;
 		 for (i=0;i < 9;++i); { 
 			 for (j=0;j < 9;++j) {
 				 grid[i][j]=0;
@@ -60,70 +58,8 @@ void output(){
 			}
 		}
 	}
-int compare(const int * a,const int * b){
-  if (*a==*b)
-    return 0;
-  else
-    if (*a < *b)
-        return -1;
-     else
-      return 1;
-}
 
-/* char check9(int array[]){
-int i;
-	----- Declarations ----- 
-	qsort(array, 9, sizeof(int),compare);  /* find out why compare wont work 
-	for (i=0;i<9;++i){
-			 if((array[i] != i+1))
-				{return 'F';}
-			 }
-	return 'T';
-}  */ 
 
-/* char gridCheck(void){
-	 char flagRow = 'T',flagCol = 'T', flagBlock = 'T';
-	 int i = 0, j=0,k=0,count = 0;  //Declaring Counters (i for row, j for column)
-	 int tempRow[9], tempCol[9], tempBlock[9]; //Temporary Variables for Row and Columns
-	 for (i = 0;i<9;++i)
-	 {
-		for (j=0;j<9;++j)
-		{
-			tempRow[j] = tempGrid[i][j]; //Saves all elements in row i to tempRow
-			tempCol[j] = tempGrid[j][i];//Saves all elements in column i to tempCol
-		}
-		//
-		flagRow = check9(tempRow);
-		flagCol = check9(tempCol);
-		if (flagRow == 'F' || flagCol == 'F')
-		{return 'F'; }
-		//
-	}
-	//----- Checking the Block -----
-	for (i=1;i<9;i+=3)
-	{
-		for(j=1;j<9;j+=3)
-		{
-			tempBlock[0] = tempGrid[i-1][j-1];
-			tempBlock[1] = tempGrid[i][j-1];
-			tempBlock[2] = tempGrid[i+1][j-1];
-			tempBlock[3] = tempGrid[i-1][j];
-			tempBlock[4] = tempGrid[i][j];
-			tempBlock[5] = tempGrid[i+1][j];
-			tempBlock[6] = tempGrid[i-1][j+1];
-			tempBlock[7] = tempGrid[i][j+1];
-			tempBlock[8] = tempGrid[i+1][j+1];
-			
-			flagBlock = check9(tempBlock);
-			if (check9(tempBlock) == 'F')
-			{
-				return 'F';
-			}	
-		}
-	}
-	return 'T';
-}
-*/ 
 void inputSudoku(void){
 	int temp[9] = {0,0,0,0,0,0,0,0,0}, i = 0, j=0;
 	FILE *puzzle = fopen("Puzzle.txt", "r");
@@ -148,8 +84,6 @@ int getCandidates(int row, int col, int* cands){
 	int temp[9];
 	int i = 0, j = 0, k = 0,array[9] = {0,0,0,0,0,0,0,0,0}, tempCol[9] = {0,0,0,0,0,0,0,0,0}, tempBox[9] = {0,0,0,0,0,0,0,0,0}, tempRow[9] = {0,0,0,0,0,0,0,0,0};
 	memcpy(tempRow,tempGrid[row],sizeof(tempRow));
-	// getRow(row, tempRow);
-	// memcpy(tempCol,tempGrid[][col],sizeof(tempCol);
 	getCol(col, tempCol);
 	getBox(row,col,tempBox);
 
@@ -159,9 +93,7 @@ int getCandidates(int row, int col, int* cands){
 		if (tempBox[i] > 0) {array[tempBox[i] - 1] = tempBox[i];}
 		
 	}
-	
-	//printf("\n %i %i %i %i %i %i %i %i %i\n",array[0],array[1],array[2],array[3],array[4],array[5],array[6],array[7],array[8]);
-	
+
 	for (i=0;i<9;++i){
 		if (array[i] == 0){
 		++j;
@@ -200,13 +132,8 @@ void getBox(int row, int col, int *tempBlock){
 			tempBlock[8] = tempGrid[i+1][j+1];	
 	
 	}
-void getRow(int row, int *tempRow){
-int i;
-for (i=0;i<9;++i) tempRow[i] = tempGrid[row][i];
-}
-	/* Implement...
 
-	*/
+	
 int bruteForce(int row,int col){
 	// ----- Declarations -----
 	int i=0, flag = 1, candFlag = 1,blankFlag = 1, nextPos[2] = {0,0};
